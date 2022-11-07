9da847b5d542cf9e723a720a0ec8c2412b9ff266
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-11-07 10:21:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var widgets = document.getElementById("widgets").innerHTML;
 
 function startHere(){
+  widgets = parseInt(widgets)
   widgets = widgets + 1;
   document.getElementById("widgets").innerHTML = widgets;
 }
