f61017545544d09c3bf9c81a268541033b44a7c9
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-11-08 10:35:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 var widgets = document.getElementById("widgets").innerHTML;
-
+var rate = 1;
 function startHere(){
   widgets = parseInt(widgets)
-  widgets = widgets + 1;
+  widgets = widgets + rate;
   document.getElementById("widgets").innerHTML = widgets;
   shouldShow();
 }
 function shouldShow(){
+  //This will detect if the user has 50 widgets
+  //When they do we show another button
   if(widgets >= 50){
     document.getElementById("secondBtn").removeAttribute("hidden")
   }
+}
+function buttonTwo(){
+  //take away 50 widgets
+  //dbl the click rate
+  widgets = parseInt(widgets);
+  widgets = widgets - 50;
+  rate = rate * 2
 }
